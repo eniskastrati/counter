fe5f603fcce3d326c8322098120c130017c00c0e
eniskastrati <enis@smartmile.eu>
2022-04-20 10:32:22 +0200

    Getting elements by id

---
+++ index.js
@@ -0,0 +1,4 @@
+const score = document.getElementById('score')
+const historyDisplay = document.getElementById('history')
+const increase_button = document.getElementById('increase')
+const save_button= document.getElementById('save')
