d1907a7b434c1d8f08db62adefa000f13d70d20a
eniskastrati <enis@smartmile.eu>
2022-04-20 10:32:49 +0200

    Declaring needed variables

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ const score = document.getElementById('score')
 const historyDisplay = document.getElementById('history')
 const increase_button = document.getElementById('increase')
 const save_button= document.getElementById('save')
+
+//variable declaration
+let counter = 0
+let history = "";
